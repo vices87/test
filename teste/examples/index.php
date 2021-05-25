15f8934e6336d9f33dee0dfad95626d8776759aa
Vicente <vcmcs@hotmail.com>
2021-05-25 19:33:28 -0300

    2

--- teste/examples/index.php
+++ teste/examples/index.php
@@ -1,28 +1,27 @@
 <?php
-
-$servername = "localhost";
-$username = "user";
-$password = "password";
-$dbname = "db";
+$servername = "172.19.0.2";
+$username = "root";
+$password = "root";
+$dbname = "test";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = mysqli_query($conn, $sql);
+$result = $conn->query($sql);
 
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } 
 
-mysqli_close($conn);
+
+
 ?>
